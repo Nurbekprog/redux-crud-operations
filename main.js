b816f23fef1c10d2d3c7743b7c463f543a0f5530
Nurbekprog <nurprogdata@gmail.com>
2024-04-08 19:18:26 +0500

    sdfd

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const redux = require("redux"); // COMMONS JS MODULE
+const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -16,19 +20,28 @@ function buyIceCream() {
   };
 }
 
-const initialState = {
+const initialCakeState = {
   numOfCakes: 10,
+};
+
+const initialIceCreamState = {
   numOfIceCreams: 20,
 };
 
-// (previousState, action) => nextState
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
@@ -39,12 +52,15 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state: ", store.getState());
 
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated state: ", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
